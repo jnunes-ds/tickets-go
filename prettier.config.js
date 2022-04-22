b5d3ce51ebd8bfd3324b8b2617704c91ab935166
Júnior Nunes <denisonprobable13@gmail.com>
2022-04-22 10:30:31 -0300

    eslint, prettier and editorconfig

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    semi: true, //ponto e vírgula no final
+    singleQuote: true, //Áspas simples
+    arrowParens: 'avoid', // Necessidade de parênteses para arrowFunction com um parâmetro
+    trailingComma: 'none', //Manter a vírgula no final mesmo que não haja um próximo item no objeto
+    emdOfLine: 'auto' // distanciamento das linhas.
+}
