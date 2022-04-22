ecf84261fe9b32f37766b9127ba1edc1a464f594
Júnior Nunes <denisonprobable13@gmail.com>
2022-04-22 11:08:11 -0300

    react-navigation installed

--- App.tsx
+++ App.tsx
@@ -11,6 +11,7 @@ import {
 	Lato_400Regular,
 	Lato_700Bold
 } from '@expo-google-fonts/lato';
+import { Routes } from './src/routes';
 
 export default function App() {
 	const [fontsLoaded] = useFonts({
@@ -34,18 +35,7 @@ export default function App() {
 	}
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
