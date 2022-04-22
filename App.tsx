e405db5c57a2b05b3343b82bf90a0c1be2ebb4d9
Júnior Nunes <denisonprobable13@gmail.com>
2022-04-22 10:57:14 -0300

    expo-app-loading && expo-google-fonts installed

--- App.tsx
+++ App.tsx
@@ -1,8 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
 import 'react-native-gesture-handler';
+import {
+	useFonts,
+	Roboto_500Medium,
+	Roboto_700Bold
+} from '@expo-google-fonts/roboto';
+import {
+	Lato_400Regular,
+	Lato_700Bold
+} from '@expo-google-fonts/lato';
 
 export default function App() {
+	const [fontsLoaded] = useFonts({
+		Roboto_500Medium,
+		Roboto_700Bold,
+		Lato_400Regular,
+		Lato_700Bold
+	});
+
+	if (!fontsLoaded) {
+		return (
+			<>
+				{
+					Platform.OS === 'android'
+					//@ts-ignore
+					? <AppLoading />
+					: <SafeAreaView />
+				}
+			</>
+		)
+	}
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
